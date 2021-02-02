a0b71028eba410fbf4666e3523144956300fa480
Jason Moore <moorejbm@gmail.com>
2021-02-02 15:07:13 -0600

    update2

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private readonly double _balance;
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine("Simulating a deposit of {} to your account");
+        }
+
+      
+        }
+    }
+}
